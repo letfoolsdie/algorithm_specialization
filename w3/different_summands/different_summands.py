b059fa87d69e59fd8715343a8cb478f13246ae03
Nikolay <letfoolsdie@gmail.com>
2016-03-21 23:21:21 +0300

    Corrected solution for Different Summands

--- w3/different_summands/different_summands.py
+++ w3/different_summands/different_summands.py
@@ -6,11 +6,11 @@ def optimal_summands(n):
         return [1]
     summands = []
     for i in range(1,n+1):
-        if n >= i*2:
+        if n > i*2:
             summands.append(i)
             n -= i
         else:
-            summands[-1] += n
+            summands.append(n)
             break
     #write your code here
     return summands
