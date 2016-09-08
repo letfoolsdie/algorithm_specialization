b4bbb930a281b0a4138acd17ce0f9f840d361f68
Nikolay <letfoolsdie@gmail.com>
2016-09-09 00:36:02 +0300

    solved first problem w4

--- w4/bin_search.py
+++ w4/bin_search.py
@@ -5,6 +5,17 @@ Created on Thu Sep  8 15:58:33 2016
 
 @author: Nikolay_Semyachkin
 """
+def bin_search(arr, hi, lo, key):
+    if hi < lo:
+        return -1
+    mid = int(lo + (hi - lo) / 2)
+    if key == arr[mid]:
+        return mid
+    elif key < arr[mid]:
+        return bin_search(arr, mid-1, lo, key)
+    else:
+        return bin_search(arr, hi, mid+1, key)
+
 
 #inp1 = '5 1 5 8 12 13'
 #inp2 = '5 8 1 23 1 11'
@@ -13,11 +24,12 @@ inp2 = input()
 
 arr = [int(i) for i in inp1.split()[1:]]
 keys = [int(i) for i in inp2.split()[1:]]
+hi = len(arr) - 1
+lo = 0
 
-#for k in keys:
-#    if k in arr:
-#        print(arr.index(k), '', end="")
-#    else:
-#        print(-1, '', end="")
+for k in keys:
+    print(bin_search(arr, hi, lo, k), '', end="")
 
-# well, that didn't work
+
+
+    
